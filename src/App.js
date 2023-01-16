4ea0a9a07d751cc95207f5bb956adf0900bd96dc
Zaid <freeze4allems@gmail.com>
2023-01-15 20:42:11 -0400

    Finished Project Using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='advice'>
+        <div className="advice__container">
+          <h6 className="advice__no">Advice #117</h6>
+          <p className="advice__text">"It is easy to sit up and take notice, what's difficult is getting up and taking action"</p>
+          <picture >
+            <source media="(min-width:650px)" srcset="pattern-divider-desktop.svg" />
+            <source media="(min-width:465px)" srcset="pattern-divider-mobile.svg" />
+            <img className="advice__img" src="pattern-divider-mobile.svg" alt="Pattern Divider" />
+          </picture>
+        </div>
+        <div className='advice__absolute'>
+          <picture>
+            <img src="icon-dice.svg" alt="Icon Dice" />
+          </picture>
+        </div>
+      </div>
     </div>
   );
 }
